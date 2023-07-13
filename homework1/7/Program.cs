3f3ff3c7820bcbe353ec2d5c2323d63691552fe6
elena2480 <136686904+elena2480@users.noreply.github.com>
2023-07-13 16:19:38 +0600

    C# Homework1 second commit

---
+++ homework1/7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.Clear();
+Console.Write("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+int b = Math.Abs(a % 10);
+Console.Write($"{b} является третьей цифрой числа {a}");
+
