0d5ca2b9e70cc8e1722c10df46af7772c7e60df8
Lukashova Elena <a240680@gmail.com>
2023-07-25 19:53:57 +0600

    hw9

---
+++ homework9/64/Program.cs
@@ -0,0 +1,16 @@
+﻿/*  Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"   */
+
+Console.Clear();
+Console.Write("Введите N: ");
+int n=int.Parse(Console.ReadLine());
+
+Console.WriteLine(PrintNumbers(1,n));
+
+string PrintNumbers(int start, int end)
+{
+    if (end == start) return end.ToString();
+    
+    return (end + " " + PrintNumbers(start, end-1));
+}   
---
+++ homework9/68/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Clear();
+Console.Write("Введите число m>0: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число n>0: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{MyAckerman(m,n)}");
+
+int MyAckerman(int mum,int num)
+{
+    if (mum == 0) return (num +1);
+      else
+        if ((mum != 0)&&(num == 0))
+            return MyAckerman((mum-1),1);
+        else
+            return MyAckerman((mum-1),MyAckerman(mum,(num-1)));
+}
